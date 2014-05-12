2d62962b6b3fcf4b60ab2ac47971f65487f66f25
Sroop <sroopsunar@hotmail.com>
2014-05-12 18:05:23 +0100

    added spock and lizard into the mix, reworked the hash to include an array of wins and amended the if else statement using indexof

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,9 +1,3 @@
-// var PAIRS = {
-//  	'rock' : { 'beats' : 'scissors'},
-//  	'paper' : { 'beats' : 'rock'},
-//  	'scissors' : { 'beats' : 'paper'}
-// }
-
 function Player(name) {
 	this.name = name;
 }
@@ -17,19 +11,21 @@ Game.prototype.winner = function(){
 	if(this.player1.pick === this.player2.pick){
 		return null;
 	}
-	else if(this.PAIRS[this.player1.pick]['beats'] === this.player2.pick){
-		return this.player1;
+	else if(this.PAIRS[this.player1.pick]['beats'].indexOf(this.player2.pick) === -1 ){
+		return this.player2;
 	}
 
 	else{
-		return this.player2;
+		return this.player1;
 	}
 }
 
 Game.prototype.PAIRS = {
-	'rock' : { 'beats' : 'scissors'},
- 	'paper' : { 'beats' : 'rock'},
- 	'scissors' : { 'beats' : 'paper'}
+	'rock' : { 'beats' : [ 'lizard' , 'scissors' ] },
+ 	'paper' : { 'beats' : ['spock' , 'rock'] },
+ 	'scissors' : { 'beats' : ['lizard' , 'paper'] },
+ 	'lizard' : { 'beats' : ['spock' , 'paper'] },
+ 	'spock' : { 'beats' : ['rock' , 'scissors'] },
 }
 
 Player.prototype.picks = function(pick){
@@ -39,4 +35,3 @@ Player.prototype.picks = function(pick){
 Game.prototype.samePick = function(){
 	return this.player1.pick === this.player2.pick;
 }
-
