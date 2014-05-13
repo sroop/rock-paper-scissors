d05ca3c4d75ac5f456e57d6e2c7fa236d4de5974
Sroop <sroopsunar@hotmail.com>
2014-05-13 12:22:12 +0100

    changed the pairs hash to include victory verbs to be used on the front end later, and also changed the winner function to reflect this change

--- javascript/lib/Game.js
+++ javascript/lib/Game.js
@@ -7,27 +7,26 @@ Game.prototype.samePick = function() {
 	return this.player1.pick === this.player2.pick;
 }
 
-Game.prototype.playerOneHasLost = function() {
-	return this.PAIRS[this.player1.pick]['beats'].indexOf(this.player2.pick) === -1;
+Game.prototype.PAIRS = {
+	'rock' : { 'lizard' : 'crushes',   'scissors' : 'blunts' },
+ 	'paper' : { 'spock' : 'disproves' , 'rock' : 'covers' },
+ 	'scissors' : { 'lizard' : 'decapitate', 'paper' : 'cut' },
+ 	'lizard' : { 'spock' : 'poisons' , 'paper' : 'eats' },
+ 	'spock' : { 'rock' : 'vaporizes', 'scissors' : 'smashes' },
 }
 
-Game.prototype.winner = function(){
+Game.prototype.victoryVerb = function(playerOnePick, playerTwoPick) {
+	return this.PAIRS[playerOnePick][playerTwoPick]
+}
+
+Game.prototype.winner = function() {
 	if(this.samePick()) {
 		return null;
 	}
-	else if(this.playerOneHasLost()) {
-		return this.player2;
-	}
-
-	else{
+	else if(this.victoryVerb(this.player1.pick, this.player2.pick)) {
 		return this.player1;
 	}
-}
-
-Game.prototype.PAIRS = {
-	'rock' : { 'beats' : [ 'lizard' , 'scissors' ] },
- 	'paper' : { 'beats' : ['spock' , 'rock'] },
- 	'scissors' : { 'beats' : ['lizard' , 'paper'] },
- 	'lizard' : { 'beats' : ['spock' , 'paper'] },
- 	'spock' : { 'beats' : ['rock' , 'scissors'] },
+	else {
+		return this.player2;
+	}
 }
